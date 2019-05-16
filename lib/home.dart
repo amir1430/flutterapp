33ddefde67a2f2812cc4c3b393f2f1d742bf25ed
amir1430 <46561556+amir1430@users.noreply.github.com>
2019-05-16 14:03:40 +0430

    Update home.dart

--- lib/home.dart
+++ lib/home.dart
@@ -100,11 +100,68 @@ class Home extends StatelessWidget {
                             width: 300,
                             child: ListTile(
                               leading: CircleAvatar(
+                                backgroundColor: Colors.deepOrangeAccent,
                                 backgroundImage: NetworkImage(
                                     'https://s3.amazonaws.com/37assets/svn/1065-IMG_2529.jpg'),
                               ),
                               title: Text('Amir'),
                               subtitle: Text('3 minute age'),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                }),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              margin: EdgeInsets.only(left: 20, top: 20),
+              child: Text(
+                'iPHONE',
+                style: TextStyle(
+                  color: Colors.deepOrangeAccent,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 15,
+                ),
+              ),
+            ),
+          ),
+          SliverToBoxAdapter(
+            child: Container(
+              margin: EdgeInsets.only(left: 10),
+              height: 400,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                children: List.generate(5, (int i) {
+                  return Container(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: <Widget>[
+                        Flexible(
+                            flex: 5,
+                            child: Container(
+                              margin: EdgeInsets.all(5),
+                              child: Image.asset(
+                                'assets/$i.jpg',
+                                fit: BoxFit.fill,
+                              ),
+                              width: 300,
+                            )),
+                        Flexible(
+                          flex: 1,
+                          child: Container(
+                            width: 300,
+                            child: ListTile(
+                              leading: CircleAvatar(
+                                backgroundColor: Colors.deepOrangeAccent,
+                                backgroundImage: NetworkImage(
+                                    'https://static-cdn.jtvnw.net/jtv_user_pictures/aa68742d-ae1f-4fb7-9d0c-e1756d5204b0-profile_image-300x300.jpg'),
+                              ),
+                              title: Text('Ali'),
+                              subtitle: Text('5 minute age'),
                             ),
                           ),
                         ),
